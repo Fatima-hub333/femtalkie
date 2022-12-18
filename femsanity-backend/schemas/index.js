761a8041a0e58b34f40d4cc62a6331239f983145
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-18 18:04:52 +0500

    create schemas

--- femsanity-backend/schemas/index.js
+++ femsanity-backend/schemas/index.js
@@ -1,1 +1,6 @@
-export const schemaTypes = []
+import post from './post';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
+
+export const schemaTypes = [post, user, comment, postedBy]
