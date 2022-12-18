0724708968e14ac244c00b5f28a798df6d77ec68
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-18 18:36:09 +0500

    Updated dependencies

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,9 @@ import type { NextPage } from 'next'
 
 export default function Home() {
   return (
-    <div>
-      FemTalkie
-   </div>
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
   )
 }
