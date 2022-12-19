88c0c2b93d95edde7848ca9aad5e15a5d91202cc
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-19 13:50:10 +0500

    Completed Discover component

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "745675affebd932a"; }
+/******/ 		__webpack_require__.h = function() { return "1f82be15f097d60c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
