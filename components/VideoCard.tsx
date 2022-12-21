2bb4582de081fb0cdd00e0a57f0d4f43be0b8e9f
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-12-21 14:35:19 +0500

    Update File

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -48,14 +48,14 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
 
       <div className="lg:ml-20 flex gap-4 relative">
         <div className='rounded-3xl'>
-          {/* <Link href="/">
+          <Link href="/">
             <video
               loop
               className='lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
               src={post.video.asset.url}
             >
             </video>
-          </Link> */}
+          </Link>
         </div>
       </div>
     </div>
